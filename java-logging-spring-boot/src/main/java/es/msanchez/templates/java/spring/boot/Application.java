e9e9f6c8a59c8c806931dab46befe314fabb60b5
Mario Codes <mariocodes1993@gmail.com>
2020-05-18 19:29:59 +0200

    logger: Implemented logger

--- java-logging-spring-boot/src/main/java/es/msanchez/templates/java/spring/boot/Application.java
+++ java-logging-spring-boot/src/main/java/es/msanchez/templates/java/spring/boot/Application.java
@@ -3,8 +3,11 @@ package es.msanchez.templates.java.spring.boot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 public class Application {
+
+  private static final Logger log = LogManager.getLogger(Application.class);
 
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
